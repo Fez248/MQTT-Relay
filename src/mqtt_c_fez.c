6b36ab9a8db79f25b542ff36744127f2caccee65
Ferran Benéitez <ferran.beneitez@fidesing.com>
2025-08-27 13:31:15 +0200

    Check for first env var

--- src/mqtt_c_fez.c
+++ src/mqtt_c_fez.c
@@ -1,5 +1,22 @@
+#define _GNU_SOURCE
 #include "mqtt_c_fez.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+#define handle_error(msg) { \
+  errno = -1; \
+  perror(msg); \
+  exit(-1); \
+}
 
 int init() {
+  printf("Configuring library...\n");
 
+  char *address = secure_getenv("ADDRESS");
+  if (!address)
+    handle_error("Address is null");
+  
+  printf("Address: %s\n", address);
 }
+
