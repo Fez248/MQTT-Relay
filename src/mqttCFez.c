f053d73f62ae95c406cd287395ead156ceaca74c
Ferran Benéitez <ferran.beneitez@fidesing.com>
2025-09-15 11:02:45 +0200

    Remove unused code from init function

--- src/mqttCFez.c
+++ src/mqttCFez.c
@@ -89,60 +89,48 @@ int init() {
 
   address = secure_getenv("ADDRESS");
   if (!address)
-    handle_error("ADDRESS is null");
+    handle_error("ADDRESS is NULL");
   printf("ADDRESS: %s\n", address);
      
   client_ID = secure_getenv("CLIENT_ID");
   if (!client_ID)
-    handle_error("CLIENT_ID is null");
+    handle_error("CLIENT_ID is NULL");
   printf("CLIENT_ID: %s\n", client_ID);
 
   pre_topic_device = secure_getenv("PRE_TOPIC_DEVICE");
   if (!pre_topic_device)
-    handle_error("PRE_TOPIC_DEVICE is null");
+    handle_error("PRE_TOPIC_DEVICE is NULL");
   printf("PRE_TOPIC_DEVICE: %s\n", pre_topic_device);
 
   pre_topic_server = secure_getenv("PRE_TOPIC_SERVER");
   if (!pre_topic_server)
-    handle_error("PRE_TOPIC_SERVER is null");
+    handle_error("PRE_TOPIC_SERVER is NULL");
   printf("PRE_TOPIC_SERVER: %s\n", pre_topic_server);
 
-  n_topics = atoi(secure_getenv("N_TOPICS"));
-  if (n_topics == 0)
-    handle_error("N_TOPICS is null");
+  char *n_topics_string = secure_getenv("N_TOPICS");
+  if (!n_topics_string)
+    handle_error("N_TOPICS is NULL");
+  n_topics = atoi(n_topics_string);
+  if (n_topics <= 0)
+    handle_error("N_TOPICS must be > 0");
   printf("N_TOPICS: %i\n", n_topics);
 
   ca_cert = secure_getenv("CA_CERT");
   if (!ca_cert)
-    handle_error("CA_CERT is null");
+    handle_error("CA_CERT is NULL");
   printf("CA_CERT: %s\n", ca_cert);
   
   cli_cert = secure_getenv("CLI_CERT");
   if (!cli_cert)
-    handle_error("CLI_CERT is null");
+    handle_error("CLI_CERT is NULL");
   printf("CLI_CERT: %s\n", cli_cert);
 
   cli_key = secure_getenv("CLI_KEY");
   if (!cli_key)
-    handle_error("CLI_KEY is null");
-  printf("CLI_KEy: %s\n", cli_key);
+    handle_error("CLI_KEY is NULL");
+  printf("CLI_KEY: %s\n", cli_key);
 
-  /*
-  // Lets do this come on
-  char *aux = malloc(aux_Size);
-  topicsAndVars = malloc(sizeof(char *) * (long unsigned int) n_topics);
-  for (int i = 1; i <= (int) n_topics; ++i) {
-    sprintf(aux, "VARS_TOPIC_%i", i);
-    topicsAndVars[i - 1] =           secure_getenv(aux);
-    if (!*topicsAndVars)
-      handle_error("%s is null\n");
-    printf("%s: %s\n", aux, topicsAndVars[i - 1]);
-  }
-  */
   printf("Library configured correctly\n");
   load();
-  // Lets do this one more time ohohoho
-  // Starships were meant to flyy
-  // Hands up and touch the skyy
 }
 
