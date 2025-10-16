65d52559ace34617d86de1cdd212c449462a4e1d
Ferran <beboferran@gmail.com>
2025-10-16 17:20:07 +0200

    createKey

--- src/msgQueue.c
+++ src/msgQueue.c
@@ -1,9 +1,19 @@
 #include "msgQueue.h"
 
-int main() {
-  struct msg_msgbuf bufSnd, bufRcv;
-  int msqidSnd, msqidRcv;
+int createKey(int keys[2]) {
   key_t keySnd, keyRcv;
+  
+  if ((keySnd = ftok("/home", 'S')) == -1) {
+    perror("ftok");
+    return -1;
+  }
 
+  if ((keyRcv == ftok("/home", 'R')) == -1) {
+    perror("ftok");
+    return -1;
+  }
 
+  if ((
 }
+
+// struct msg_msgbuf bufSnd, bufRcv;
